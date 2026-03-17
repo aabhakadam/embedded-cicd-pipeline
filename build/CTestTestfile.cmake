a63ad533a3f91601a4afe4c9c33dbf38ff9dd063
Aabha Kadam <aabhakadam567@gmail.com>
2026-03-17 23:20:25 +0100

    feat: embedded CI/CD pipeline with GoogleTest and GitHub Actions

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/embedded-cicd-pipeline
-# Build directory: /root/embedded-cicd-pipeline/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SensorTests "/root/embedded-cicd-pipeline/build/run_tests")
-set_tests_properties(SensorTests PROPERTIES  _BACKTRACE_TRIPLES "/root/embedded-cicd-pipeline/CMakeLists.txt;27;add_test;/root/embedded-cicd-pipeline/CMakeLists.txt;0;")
